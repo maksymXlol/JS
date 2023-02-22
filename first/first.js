42f21c0dba7f155c0a29374f54ddcd38f146dcd1
maksymXlol <124809697+maksymXlol@users.noreply.github.com>
2023-02-22 17:28:44 +0000

    First commit

---
+++ first/first.js
@@ -0,0 +1,18 @@
+// alert("welcome")
+// prompt("What is your name?")
+// confirm("Did you see my another website?")
+// console.log("something")
+
+let myName = "Max";
+let age = 11;
+let birthYear = 2022 - age;
+let isPupil = true;
+age = 12;
+
+let x = 54;
+let y = 17;
+
+const pi = 3.14;
+// pi = 15; // error : you can't change const
+
+console.log(pi);
